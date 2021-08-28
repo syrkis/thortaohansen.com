9c7ebd5cce0ac81e5688cad7588c56dc56395c5e
Noah Syrkis <noah@syrkis.com>
2021-08-28 18:49:30 +0200

    reorder works

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,12 +1,12 @@
 <script>
     let routes = [
+        {'url': 'seven', 'title': 'Seven sages laid it\'s foundations'},
+        {'url': 'sun', 'title': 'The sun hitches up her skirt for Utnapishtim'},
+        {'url': 'fuzzy', 'title': 'Fuzzy is the language of creation'},
+        {'url': 'garnitures', 'title': 'Garnitures for temple or house'},
+        {'url': 'haben', 'title': 'Haben sie bigbag (Ecstacy of Saint Sebastian)'},
         {'url': 'lilt', 'title': 'Lilt Series'},
         {'url': 's_90_3', 'title': 'S 90 3'},
-        {'url': 'haben', 'title': 'Haben sie bigbag (Ecstacy of Saint Sebastian)'},
-        {'url': 'garnitures', 'title': 'Garnitures for temple or house'},
-        {'url': 'fuzzy', 'title': 'Fuzzy is the language of creation'},
-        {'url': 'sun', 'title': 'The sun hitches up her skirt for Utnapishtim'},
-        {'url': 'seven', 'title': 'Seven sages laid it\'s foundations'},
         {'url': '1UU', 'title': '1UU'},
         {'url': 'other', 'title': 'other'},
         {'url': 'contact', 'title': 'Contact'},
